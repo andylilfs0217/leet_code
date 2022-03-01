c88b42ab49ad6ef22bcdf17759dce6e5fde63786
Andy Li <andylilfs@gmail.com>
2022-03-01 10:07:08 +0800

    update

--- medium/0015_3sum.py
+++ medium/0015_3sum.py
@@ -7,20 +7,26 @@ class Solution:
         return self.threeSum1(nums)
 
     def threeSum1(self, nums: List[int]) -> List[List[int]]:
-        counter = Counter(nums)
-        keys = list(counter)
-        n = len(keys)
         res = []
-        for i in range(n):
-            for j in range(i, n):
-                key_i, key_j = keys[i], keys[j]
-                counter[key_i] -= 1
-                counter[key_j] -= 1
-                key_k = 0-key_i-key_j
-                if counter.get(key_k, 0) > 0 and counter[key_j] >= 0:
-                    res.append([key_i, key_j, key_k])
-                counter[key_i] += 1
-                counter[key_j] += 1
+        nums.sort()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
         return res
 
 
---
+++ medium/0148_sort_list.py
@@ -0,0 +1,54 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        # Split the list into two halfs
+        left = head
+        right = self.getMid(head)
+        tmp = right.next
+        right.next = None
+        right = tmp
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+
+        return self.merge(left, right)
+
+    def getMid(self, head):
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    # Merge the list
+    def merge(self, list1, list2):
+        newHead = tail = ListNode()
+        while list1 and list2:
+            if list1.val > list2.val:
+                tail.next = list2
+                list2 = list2.next
+            else:
+                tail.next = list1
+                list1 = list1.next
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        if list2:
+            tail.next = list2
+
+        return newHead.next
