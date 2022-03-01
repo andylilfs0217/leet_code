377215138570a149766abb6502532936b322a63f
Andy Li <andylilfs@gmail.com>
2022-03-01 16:19:46 +0800

    update

---
+++ medium/0117_populating_next_right_pointers_in_each_node_ii.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        prev_layer = [root]
+        curr_layer = []
+        while len(prev_layer) > 0 or len(curr_layer) > 0:
+            while len(prev_layer) > 0:
+                curr_root = prev_layer.pop(0)
+                # TODO
