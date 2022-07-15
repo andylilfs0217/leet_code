90d58028df0245c61d5a67d283f727cc053478a4
Andy Li <andylilfs@gmail.com>
2022-07-15 08:52:40 +0800

    0695

--- medium/0695_max_area_of_island.py
+++ medium/0695_max_area_of_island.py
@@ -3,7 +3,8 @@ from typing import List
 
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        return self.maxAreaOfIsland1(grid)
+        # return self.maxAreaOfIsland1(grid)
+        return self.maxAreaOfIsland2(grid)
 
     def maxAreaOfIsland1(self, grid: List[List[int]]) -> int:
         def dfs(i: int, j: int) -> int:
@@ -24,6 +25,32 @@ class Solution:
                     continue
                 area = dfs(i, j)
                 max_area = max(area, max_area)
+        return max_area
+
+    def maxAreaOfIsland2(self, grid: List[List[int]]) -> int:
+        max_area = 0
+        directions = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        height, width = len(grid), len(grid[0])
+
+        def bfs(i: int, j: int) -> int:
+            queue = [(i, j)]
+            temp_counter = 0
+            for x, y in queue:
+                if x >= 0 and y >= 0 and x < height and y < width:
+                    cell = grid[x][y]
+                    if cell == 1:
+                        temp_counter += 1
+                        grid[x][y] = 0
+                        for di, dj in directions:
+                            queue.append((x+di, y+dj))
+            return temp_counter
+
+        for i, row in enumerate(grid):
+            for j, cell in enumerate(row):
+                if cell == 0:
+                    continue
+                area = bfs(i, j)
+                max_area = max(max_area, area)
         return max_area
 
 
