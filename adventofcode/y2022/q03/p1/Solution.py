cb3e8316e443b2358f3d2195b99b24177a8de037
Andy Li <andylilfs@gmail.com>
2022-12-03 14:28:49 +0800

    2022 03

--- adventofcode/y2022/q03/p1/Solution.py
+++ adventofcode/y2022/q03/p1/Solution.py
@@ -6,9 +6,24 @@ def Solution(f: TextIOWrapper):
     lines = f.read().splitlines()
 
     # Write your codes here
+    def charToNum(c: str) -> int:
+        number = ord(c)
+        if number > 96:
+            minus = 96
+        else:
+            minus = 64 - 26
+        number = number - minus
+        return number
+
     res = 0
     for line in lines:
-        print()
+        n = len(line)
+        first, second = set(line[:n // 2]), set(line[n // 2:])
+        union = first & second
+        union_char = union.pop()
+        number = charToNum(union_char)
+        print(number)
+        res += number
 
     # Finish your codes here
     return res
--- adventofcode/y2022/q03/p2/Solution.py
+++ adventofcode/y2022/q03/p2/Solution.py
@@ -6,9 +6,23 @@ def Solution(f: TextIOWrapper):
     lines = f.read().splitlines()
 
     # Write your codes here
+    def charToNum(c: str) -> int:
+        number = ord(c)
+        if number > 96:
+            minus = 96
+        else:
+            minus = 64 - 26
+        number = number - minus
+        return number
+
     res = 0
-    for line in lines:
-        print()
+    n = len(lines)
+    for i in range(0, n, 3):
+        first, second, third = lines[i:i + 3]
+        intersect = set(first) & set(second) & set(third)
+        intersect_char = intersect.pop()
+        number = charToNum(intersect_char)
+        res += number
 
     # Finish your codes here
     return res
