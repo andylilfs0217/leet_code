b133f4ad4c28c3c8f68785f0701eec0b7e8b69b9
Andy Li <andy.fsli@lalamove.com>
2022-12-05 11:45:16 +0800

    2022 04

---
+++ adventofcode/y2022/q04/p2/Solution.py
@@ -0,0 +1,18 @@
+from enum import Enum
+from io import TextIOWrapper
+
+
+def Solution(f: TextIOWrapper):
+    lines = f.read().splitlines()
+
+    # Write your codes here
+    res = 0
+    for line in lines:
+        g1, g2 = [list(map(int, g.split('-'))) for g in line.split(',')]
+        s1, s2 = set(range(g1[0], g1[1] + 1)), set(range(g2[0], g2[1] + 1))
+        if len(s1 & s2) > 0:
+            res += 1
+        pass
+
+    # Finish your codes here
+    return res
