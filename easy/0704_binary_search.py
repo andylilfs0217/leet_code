ece3ad32d4c1ab252500dc2198b067f4abb51bc6
Andy Li <andylilfs@gmail.com>
2025-01-13 14:46:39 -0500

    lc 704

--- easy/0704_binary_search.py
+++ easy/0704_binary_search.py
@@ -4,14 +4,14 @@ from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        l, r = 0, n-1
+        l, r = 0, n - 1
         while l <= r:
-            mid_i = (r+l)//2
-            mid = nums[mid_i]
-            if mid == target:
-                return mid_i
-            elif mid < target:
-                l = mid_i + 1
+            m = (l + r) // 2
+            num = nums[m]
+            if num == target:
+                return m
+            elif num < target:
+                l = m + 1
             else:
-                r = mid_i - 1
+                r = m - 1
         return -1
