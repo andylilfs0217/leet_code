8a9facd9373b2444e674c8d554236f49e20fdfcb
Andy Li <andylilfs@gmail.com>
2025-04-14 23:57:05 -0400

    update

--- medium/0981_time_based_key_value_store.py
+++ medium/0981_time_based_key_value_store.py
@@ -1,47 +1,43 @@
+import collections
+from typing import List
+from bisect import bisect_left
+
+
 class TimeMap:
 
     def __init__(self):
-        self.hm = {}
+        """
+        {
+            'foo': [(1, 'bar'), (4, 'bar2'), (7, 'bar3')]
+        }
+        """
+        self.time_map = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        v_list = self.hm.get(key, [])
-        v_list.append((timestamp, value))
-        self.hm[key] = v_list
+        self.time_map[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        v_list = self.hm.get(key, [])
-        n = len(v_list)
-        # Linear search
-        # ans = ""
-        # for i in range(n):
-        #     t, value = v_list[i]
-        #     if timestamp >= t:
-        #         ans = value
-        #     else:
-        #         break
-        # return ans
-
-        # Binary search
-        ans = ""
-        l, r = 0, n - 1
-        while l <= r:
-            m = (l + r) // 2
-            t1, value1 = v_list[m]
-            if m + 1 >= n:
-                if timestamp >= t1:
-                    ans = value1
-                break
-            else:
-                t2, _ = v_list[m + 1]
-                if timestamp >= t1 and timestamp < t2:
-                    ans = value1
-                    break
-                elif timestamp >= t1:
-                    l = m + 1
+        def search_val_from_list(ts: int, val_list: List) -> str:
+            res = ""
+            n = len(val_list)
+            l, r = 0, n - 1
+            found_res = False
+            while l <= r and not found_res:
+                mid = (l + r) // 2
+                t, val = val_list[mid]
+                if t == ts:
+                    res = val
+                    found_res = True
+                elif t < ts:
+                    res = val
+                    l = mid + 1
                 else:
-                    r = m - 1
-                pass
-        return ans
+                    r = mid - 1
+            return res
+
+        val_hist = self.time_map[key]
+        res = search_val_from_list(timestamp, val_hist)
+        return res
 
 
 # Your TimeMap object will be instantiated and called as such:
@@ -51,14 +47,14 @@ class TimeMap:
 
 timeMap = TimeMap()
 timeMap.set("foo", "bar", 1)
-print(timeMap.get("foo", 1))
-print(timeMap.get("foo", 3))
+print(timeMap.get("foo", 1))  # bar
+print(timeMap.get("foo", 3))  # bar
 timeMap.set("foo", "bar2", 4)
-print(timeMap.get("foo", 4))
-print(timeMap.get("foo", 5))
+print(timeMap.get("foo", 4))  # bar2
+print(timeMap.get("foo", 5))  # bar2
 timeMap.set("foo", "bar3", 7)
-print(timeMap.get("foo", 8))
-print(timeMap.get("foo", 5))
-print(timeMap.get("foo", 2))
-print(timeMap.get("foo", 0))
-print(timeMap.get("bar", 3))
+print(timeMap.get("foo", 8))  # bar3
+print(timeMap.get("foo", 5))  # bar2
+print(timeMap.get("foo", 2))  # bar
+print(timeMap.get("foo", 0))  # ''
+print(timeMap.get("bar", 3))  # ''
